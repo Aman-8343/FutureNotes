591f734290e5ccad1e0ed7b4987f9ce8b9493c2c
Aman kumar <amankumar105541@gmail.com>
2025-08-21 21:45:44 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import CreateCapsule from './pages/CreateCapsule';
+import './App.css';
 
-import './App.css'
+
+const PrivateRoute = ({ children }) => {
+    const token = localStorage.getItem('token');
+    return token ? children : <Navigate to="/login" />;  // for protecting routes
+};
 
 function App() {
-  
+    return (
+        <div className="App">
+            <Routes>
+                
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
 
-  return (
-    <>
-      kk
-    </>
-  )
+                
+                <Route
+                    path="/dashboard"
+                    element={<PrivateRoute><Dashboard /></PrivateRoute>}
+                />
+                <Route
+                    path="/create"
+                    element={<PrivateRoute><CreateCapsule /></PrivateRoute>}
+                />
+
+               
+                <Route path="/" element={<Navigate to="/dashboard" />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
