458b7e483d09931759d484ba0d5f196850b8fad5
Aman kumar <amankumar105541@gmail.com>
2025-08-23 13:10:22 +0530

    enable email service

--- server/server.js
+++ server/server.js
@@ -17,28 +17,28 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
-// Serve uploaded files statically
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Database Connection
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// API Routes
+
 app.use('/api/auth', authRoutes);
 app.use('/api/capsules', capsuleRoutes);
 
 // Scheduled Task: Check for capsules to unlock every day 
-// cron.schedule('0 0 * * *', () => {
-//     console.log('Running daily check for capsule unlocks...');
-//     checkAndSendNotifications();
-// }, {
-//     timezone: "Asia/Kolkata" 
-// });
+cron.schedule('0 0 * * *', () => {
+    console.log('Running daily check for capsule unlocks......');
+    checkAndSendNotifications();
+}, {
+    timezone: "Asia/Kolkata" 
+});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
